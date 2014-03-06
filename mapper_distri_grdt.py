4c1c51dd8991299019323ed6de239ef0a7b501e2
root <root@debian>
2014-03-06 12:02:58 +0100

    mapper_distri corrected

--- mapper_distri_grdt.py
+++ mapper_distri_grdt.py
@@ -7,14 +7,18 @@ import numpy as np
 
 p = len(sys.argv)
 grad = np.asmatrix(np.repeat(0,(p-1)).astype(np.float))
+loglikelihood = 0
+
+beta = np.matrix([float(i) for i in sys.argv[1:]])
 
 for line in sys.stdin:
 	line = line.strip()
 	line = line.split()
 	line = [float(i) for i in line]
-
+	
+	y = line[0]
 	x = np.matrix(line[1:p])
-	beta = np.matrix([float(i) for i in sys.argv[1:]])
 
-	grad += x*(line[0] - 1/(1 + exp(x*beta.T)))
+	grad += x*(y - 1/(1 + exp(x*beta.T)))
+
 print("{}\t{}".format('grad',grad))
